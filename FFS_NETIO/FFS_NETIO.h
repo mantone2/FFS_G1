115f1925880d5403b5eae6eee49c2988155bbc49
Michael K. Antonelli <michael.k.antonelli@gmail.com>
2011-02-12 01:26:19 -0800

    LGPL License

--- FFS_NETIO/FFS_NETIO.h
+++ FFS_NETIO/FFS_NETIO.h
@@ -1,6 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+//            Copyright (C) 2011 by Four Factor Studios
+//                         All Rights Reserved
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU Lesser General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU Lesser General Public License for more details.
+//
+//	You should have received a copy of the GNU Lesser General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//		Michael Antonelli			Four Factor Studios					2011
+//		Michael Antonelli			Four Factor Studios				(C) 2011
 //
 //		FFS_NETIO is intended to be a set of high level helper functions for
 //		network communication. 
