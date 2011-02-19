b760d0b353ea651986a518a6a4c86e25586a6e84
Michael K. Antonelli <michael.k.antonelli@gmail.com>
2011-02-18 23:49:14 -0800

    First Ceiphied Commit
    Signed-off-by: Michael K. Antonelli <michael.k.antonelli@gmail.com>

---
+++ Ceiphied/Ceiphied/Animation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ceiphied
+{
+    public partial class Animation : Form
+    {
+        public Animation()
+        {
+            InitializeComponent();
+        }
+    }
+}
